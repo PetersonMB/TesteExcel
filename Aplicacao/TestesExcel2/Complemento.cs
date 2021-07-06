c4b03b087446647af7eefa5bdcaf5ee940c12231
Peterson brandao <peterson.brandao28@outlook.com>
2021-07-06 14:21:46 -0300

    Nova Aplicação

---
+++ Aplicacao/TestesExcel2/Complemento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesExcel2
+{
+    public class Complemento
+    {
+        public string CodVendedor { get; set; }
+        public string CPFVendedor { get; set; }
+        public string TipoVendedor { get; set; }
+    }
+}
---
+++ Aplicacao/TestesExcel2/Program.cs
@@ -0,0 +1,29 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesExcel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var excel = new ExcelQueryFactory(@"C:\TRINDADE\SUPORTE\Layout Mediatorie\COMISSÃO MEDIATORIE CLUBE 1 COMP 05.2021 - Editado.xlsx");
+
+            excel.AddMapping<Complemento>(x => x.CodVendedor, "COD VENDEDOR");
+            excel.AddMapping<Complemento>(x => x.CodVendedor, "CPF VENDEDOR");
+            excel.AddMapping<Complemento>(x => x.CodVendedor, "TIPO VEND");
+
+
+            List<Complemento> Testes2 = excel.Worksheet<Complemento>("COMISSÃO MEDIATORIE 05.2021").ToList();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
